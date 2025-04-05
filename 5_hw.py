3fffb812359fc490ea3c54328e044d8f16d6a3fd
@omi-coder <omi-ufa@yandex.ru>
2025-04-05 22:50:31 +0300

    added 5_hw

--- 5_hw.py
+++ 5_hw.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
 driver.get('https://www.saucedemo.com/')
@@ -10,4 +11,7 @@ icon_user_name = driver.find_element(By.CSS_SELECTOR, '#user-name')
 icon_password = driver.find_element(By.CSS_SELECTOR, '#password')
 button_login = driver.find_element(By.CSS_SELECTOR, '#login-button')
 
-print('Элементы найдены' if icon_user_name and icon_password and button_login else 'Элементы не найдены')
+try:
+    print('Элементы найдены' if icon_user_name and icon_password and button_login else 'Элементы не найдены')
+except NoSuchElementException as exc:
+    print(exc)
