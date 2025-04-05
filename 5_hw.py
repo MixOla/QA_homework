12c693a37c26ad364b63c650f5fe19d0966753e1
@omi-coder <omi-ufa@yandex.ru>
2025-04-05 22:38:48 +0300

    added README.md

---
+++ 5_hw.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('https://www.saucedemo.com/')
+
+
+# поиск элемента
+icon_user_name = driver.find_element(By.CSS_SELECTOR, '#user-name')
+icon_password = driver.find_element(By.CSS_SELECTOR, '#password')
+button_login = driver.find_element(By.CSS_SELECTOR, '#login-button')
+
+print('Элементы найдены' if icon_user_name and icon_password and button_login else 'Элементы не найдены')
